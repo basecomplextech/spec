4b4ffdcefb79834ff5b4c779fa7500f568359a75
Ivan Korobkov <ivan.korobkov@gmail.com>
2023-08-19 21:57:35 +0800

    rpc: Http client, #22

--- rpc/client.go
+++ rpc/client.go
@@ -1,9 +1,20 @@
 package rpc
 
 import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"io"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/basecomplextech/baselibrary/alloc"
 	"github.com/basecomplextech/baselibrary/status"
 	"github.com/basecomplextech/spec/proto/prpc"
 )
+
+const ContentType = "application/spec-rpc"
 
 // Clien is an RPC client.
 type Client interface {
@@ -14,9 +25,13 @@ type Client interface {
 	Request(cancel <-chan struct{}, req *Request) (prpc.Response, status.Status)
 }
 
-// NewClient returns a new client.
-func NewClient(c Connector) Client {
-	return newClient(c)
+// NewClient returns a new client with the given config.
+func NewClient(config *ClientConfig, url string) (Client, status.Status) {
+	client, st := newHttpClient(config)
+	if !st.OK() {
+		return nil, st
+	}
+	return newClient(client, url), status.OK
 }
 
 // internal
@@ -24,53 +39,146 @@ func NewClient(c Connector) Client {
 var _ Client = (*client)(nil)
 
 type client struct {
-	connector Connector
+	client *http.Client
+	url    string
 }
 
-func newClient(c Connector) *client {
+func newClient(client_ *http.Client, url string) *client {
 	return &client{
-		connector: c,
+		client: client_,
+		url:    url,
 	}
+}
+
+func newHttpClient(config *ClientConfig) (*http.Client, status.Status) {
+	if config == nil {
+		config = DefaultClientConfig()
+	}
+
+	// Dialer
+	dialer := &net.Dialer{
+		Timeout:   config.DialTimeout,
+		KeepAlive: config.DialKeepAlive,
+	}
+
+	// Proxy
+	proxy := http.ProxyFromEnvironment
+	if !config.ProxyFromEnv {
+		proxy = nil
+	}
+
+	// TLS config
+	tlsConfig := &tls.Config{}
+	{
+		mod := false
+		if config.TLSRootCert != "" {
+			cert, err := os.ReadFile(config.TLSRootCert)
+			if err != nil {
+				return nil, status.WrapErrorf(err, "Failed to read root certificate")
+			}
+
+			pool := x509.NewCertPool()
+			pool.AppendCertsFromPEM(cert)
+			tlsConfig.RootCAs = pool
+			mod = true
+		}
+
+		if config.TLSInsecureSkip {
+			tlsConfig.InsecureSkipVerify = true
+			mod = true
+		}
+
+		if !mod {
+			tlsConfig = nil
+		}
+	}
+
+	// Transport
+	trans := &http.Transport{
+		Proxy:                 proxy,
+		DialContext:           dialer.DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          config.MaxIdleConns,
+		IdleConnTimeout:       config.IdleConnTimeout,
+		TLSClientConfig:       tlsConfig,
+		TLSHandshakeTimeout:   config.TLSHandshakeTimeout,
+		ExpectContinueTimeout: config.ExpectContinueTimeout,
+	}
+
+	// Client
+	c := &http.Client{Transport: trans}
+	return c, status.OK
 }
 
 // Free releases the client.
 func (c *client) Free() {
-	c.connector.Free()
+	c.client.CloseIdleConnections()
 }
 
 // Request sends a request and returns a response.
 func (c *client) Request(cancel <-chan struct{}, req *Request) (prpc.Response, status.Status) {
 	// Build request
-	preq, st := req.Build()
-	if !st.OK() {
-		return prpc.Response{}, st
-	}
+	var req1 *http.Request
+	{
+		preq, st := req.Build()
+		if !st.OK() {
+			return prpc.Response{}, st
+		}
 
-	// Open connection
-	conn, st := c.connector.Connect(cancel)
-	if !st.OK() {
-		return prpc.Response{}, st
+		buf := bytes.NewBuffer(preq.Unwrap().Raw())
+		var err error
+
+		req1, err = http.NewRequest(http.MethodPost, c.url, buf)
+		if err != nil {
+			return prpc.Response{}, status.WrapError(err)
+		}
+
+		req1.Header.Set("Content-Type", ContentType)
 	}
-	defer conn.Free()
 
 	// Send request
-	breq := preq.Unwrap().Raw()
-	if st := conn.Send(cancel, breq); !st.OK() {
-		return prpc.Response{}, st
+	resp1, err := c.client.Do(req1)
+	if err != nil {
+		return prpc.Response{}, WrapError(err)
 	}
 
-	// Receive response
-	bresp, st := conn.Receive(cancel)
-	if !st.OK() {
-		return prpc.Response{}, st
+	// Check response
+	{
+		code := resp1.StatusCode
+		if (code / 100) != 2 {
+			return prpc.Response{}, Errorf("Unexpected response status: %v", resp1.Status)
+		}
+		ctype := resp1.Header.Get("Content-Type")
+		if ctype != ContentType {
+			return prpc.Response{}, Errorf("Unexpected response content type: %v", ctype)
+		}
+	}
+
+	// Read body
+	clen := resp1.ContentLength
+	if clen < 0 {
+		clen = 0
+	}
+	buf1 := alloc.NewBufferSize(int(clen))
+
+	ok := false
+	defer func() {
+		if !ok {
+			buf1.Free()
+		}
+	}()
+
+	_, err = io.Copy(buf1, resp1.Body)
+	if err != nil {
+		return prpc.Response{}, WrapError(err)
 	}
 
 	// Parse response
-	presp, _, err := prpc.ParseResponse(bresp)
+	presp, _, err := prpc.ParseResponse(buf1.Bytes())
 	if err != nil {
-		return prpc.Response{}, status.WrapError(err)
+		return prpc.Response{}, WrapError(err)
 	}
 
-	// Return response
+	ok = true
 	return presp, status.OK
 }
