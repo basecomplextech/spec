df5ee1ddcbc5f6758419a872bcf150f70bbe9e3d
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-10-26 12:56:36 +0800

    mpx: Reorganize files, #43

--- mpx/conn.go
+++ mpx/conn.go
@@ -785,14 +785,3 @@ func (c *connImpl) notifyClosed() {
 	}
 	clear(c.closedListeners)
 }
-
-// worker pool
-
-// workerPool allows to reuse goroutines with bigger stacks for handling channels.
-// It does not provide any performance benefits in test benchmarks, but it does provide
-// performance gains in real-world scenarios with big stacks, especially with chained RPC handlers.
-//
-//	BenchmarkTable_Get_Parallel-10:
-//	goroutines		144731 ops	568 B/op	23 allocs/op
-//	goroutine pool	184801 ops	558 B/op	23 allocs/op
-var workerPool = async.NewPool()
