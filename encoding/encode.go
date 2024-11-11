053492829f718c2ab706fd2c880f04cba3ef3c57
Ivan Korobkov <ivan.korobkov@gmail.com>
2024-11-11 17:49:11 +0800

    encoding: Update bin encode methods

--- encoding/encode.go
+++ encoding/encode.go
@@ -119,21 +119,21 @@ func EncodeBin64(b buffer.Buffer, v bin.Bin64) (int, error) {
 
 func EncodeBin128(b buffer.Buffer, v bin.Bin128) (int, error) {
 	p := b.Grow(17)
-	copy(p, v[:])
+	v.MarshalTo(p)
 	p[16] = byte(core.TypeBin128)
 	return 17, nil
 }
 
 func EncodeBin128Bytes(b buffer.Buffer, v bin.Bin128) ([]byte, int, error) {
 	p := b.Grow(17)
-	copy(p, v[:])
+	v.MarshalTo(p)
 	p[16] = byte(core.TypeBin128)
 	return p, 17, nil
 }
 
 func EncodeBin256(b buffer.Buffer, v bin.Bin256) (int, error) {
 	p := b.Grow(33)
-	copy(p, v[:])
+	v.MarshalTo(p)
 	p[32] = byte(core.TypeBin256)
 	return 33, nil
 }
